a868c451ee52c939fd709f95892a183cfb6f5285
geka.evk <geka.evk@gmail.com>
2025-03-05 19:08:06 +0000

    feat(csi-1252): improved logs

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,45 +1,53 @@
-const { createLogger, format, transports } = require('winston')
-const { combine, timestamp, colorize, printf } = format
+// const { createLogger, format, transports } = require('winston')
+// const { combine, timestamp, colorize, printf } = format
+//
+// // const { customLevels, level, logTransport, transportFileOptions } = require('./lib/config')
+//
+// const allLevels = { error: 0, warn: 1, audit: 2, trace: 3, info: 4, perf: 5, verbose: 6, debug: 7, silly: 8 }
+// // const customLevelsArr = customLevels.split(/ *, */) // extra white space before/after the comma is ignored
+// // const ignoredLevels = customLevels ? Object.keys(allLevels).filter(key => !customLevelsArr.includes(key)) : []
+//
+// interface formatInput {
+//   level: string
+//   message: string
+//   timestamp: string
+// }
+//
+// const customFormat = printf(({ level, message, timestamp }: formatInput) => {
+//   return `${timestamp} - ${level}: ${message}`
+// })
+//
+// const transport = new transports.Console()
+//
+// const Logger = createLogger({
+//   level: 'silly',
+//   levels: allLevels,
+//   format: combine(
+//     timestamp(),
+//     colorize({
+//       colors: {
+//         audit: 'magenta',
+//         trace: 'white',
+//         perf: 'green'
+//       }
+//     }),
+//     customFormat
+//   ),
+//   transports: [
+//     transport
+//   ],
+//   exceptionHandlers: [
+//     transport
+//   ],
+//   exitOnError: false
+// })
+//
+// module.exports = Logger
 
-// const { customLevels, level, logTransport, transportFileOptions } = require('./lib/config')
+const { loggerFactory } = require('@mojaloop/central-services-logger/src/contextLogger');
 
-const allLevels = { error: 0, warn: 1, audit: 2, trace: 3, info: 4, perf: 5, verbose: 6, debug: 7, silly: 8 }
-// const customLevelsArr = customLevels.split(/ *, */) // extra white space before/after the comma is ignored
-// const ignoredLevels = customLevels ? Object.keys(allLevels).filter(key => !customLevelsArr.includes(key)) : []
+const logger = loggerFactory('event-sdk');
 
-interface formatInput {
-  level: string
-  message: string
-  timestamp: string
+module.exports = {
+  logger
 }
-
-const customFormat = printf(({ level, message, timestamp }: formatInput) => {
-  return `${timestamp} - ${level}: ${message}`
-})
-
-const transport = new transports.Console()
-
-const Logger = createLogger({
-  level: 'silly',
-  levels: allLevels,
-  format: combine(
-    timestamp(),
-    colorize({
-      colors: {
-        audit: 'magenta',
-        trace: 'white',
-        perf: 'green'
-      }
-    }),
-    customFormat
-  ),
-  transports: [
-    transport
-  ],
-  exceptionHandlers: [
-    transport
-  ],
-  exitOnError: false
-})
-
-module.exports = Logger
