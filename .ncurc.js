0213343acf3bc1fca080d3350739b4277160463e
Juan Correa <gibaros@users.noreply.github.com>
2026-07-06 19:03:38 -0400

    chore: take dependency majors per no-preemptive-reject policy [mojaloop/#4479]

--- .ncurc.js
+++ .ncurc.js
@@ -1,9 +1,5 @@
 module.exports = {
   // Add a TODO comment indicating the reason for each rejected dependency upgrade added to this list, and what should be done to resolve it (i.e. handle it through a story, etc).
   reject: [
-    // TODO: 'serialize-error' version is fixed to v8.1.0, this is because v9+ only supports ESM loaders and not CJS. This will need to be addressed in a future story.
-    "serialize-error",
-    "jest", // jest 30 is released but libraries that work with jest have not been updated to support it yet.
-    "eslint" // eslint 10.x is not yet supported by @typescript-eslint 8.x (requires ^8.57.0 || ^9.0.0)
   ]
 }
